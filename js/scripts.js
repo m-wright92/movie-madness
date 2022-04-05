f743c8a2b3f82e8ea597eac64f6e62731534b1c3
Michael Wright <m.wright92@icloud.com>
2022-04-05 13:54:18 -0700

    show users selected movie and time slot
    Co-authored-by: Mark McConnell <amark.mcconnell@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,71 @@
 
+const price = 15;
 
-
-
-function MovieList() {
-  this.tickets = {}
+Ticket.prototype.mName = function(ticket) {
+  if (this.movieName === "Dune 2") {
+    console.log(price + 5);
+  } else {
+    console.log(price);
+  }
 }
 
-MovieList.prototype.addTickets = function(ticket) {
-  this.tickets[ticket.movieName] = ticket;
+Ticket.prototype.mTime = function(ticket) {
+  if (this.movieTime === "6") {
+    console.log(price + 1.5);
+  } else if (this.movieTime === "3") {
+    console.log(price - 1.5);
+  } else {
+    
+  }
 }
 
-function Ticket(movieName, category, showTime) {
+Ticket.prototype.getPrice = function(ticket) {
+  if (parseInt(this.userAge) >= 65) {
+    console.log(price * .7); 
+  }else if(parseInt(this.userAge) <= 10) {
+    console.log(price * .5);
+  }else {
+    console.log(price);
+  };
+}
+
+function Ticket(movieName, movieTime, userAge) {
   this.movieName = movieName;
-  this.category = category;
-  this.showTime = showTime;
+  this.movieTime = movieTime;
+  this.userAge = userAge;
   // this.customerAge = this.customerAge;
 }
 
 // UI Logic ----
-let movieList = new Ticket();
 
+function displayTicketInfo(ticketToDisplay) {
+  // const tickKeys = Object.keys(ticketToDisplay);
+  let ticketInfo = $("p#output");
+  let htmlForTicketInfo = ticketToDisplay.movieName + " @ " + ticketToDisplay.movieTime + "pm";
+  // tickKeys.forEach(function(key) {
+  //   // const ticket = ticketToDisplay(key);
+  //   htmlForTicketInfo += ticketToDisplay.movieName + " " + ticketToDisplay.movieTime + " ";
+  
+  ticketInfo.html(htmlForTicketInfo);
+}
+
+$(document).ready(function() {
+  $("form#movieOptions").submit(function(event) {
+    event.preventDefault();
+    const movieName = $("#movie").val();
+    const movieTime = parseInt($("#movieTime").val());
+    const userAge = $("input#age-input").val();
+    let newTick = new Ticket(movieName, movieTime, userAge);
+    displayTicketInfo(newTick);
+    // $("#output").text(newTick);
+  })
+})
+// function showMovie(movieName) {
+
+// }
+
+// function attachMovieListeners() {
+//   $("ul#movies").on("click", "li", function () {
+//     showMovie(this.movie);
+//   })
+// }
